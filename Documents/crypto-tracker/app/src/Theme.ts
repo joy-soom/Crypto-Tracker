7107662bd59852ba923a6e808806821e4af6d775
park <goslwjddl@naver.com>
2023-03-10 17:18:14 +0900

    Design : css reset 적용

--- Documents/crypto-tracker/app/src/Theme.ts
+++ Documents/crypto-tracker/app/src/Theme.ts
@@ -1,9 +1,9 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor:"tomato",
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor:"#00a8ff",
 };
 
 // export const darkTheme: DefaultTheme = {
