26a5dcede00dfcf7bde0b1c8c218d4c8b7ec0ad0
park <goslwjddl@naver.com>
2023-03-11 16:53:03 +0900

    feat : icon Api 개발

--- Documents/crypto-tracker/app/src/routes/Coins.tsx
+++ Documents/crypto-tracker/app/src/routes/Coins.tsx
@@ -21,9 +21,10 @@ const Coin = styled.li`
 
   border-radius: 15px;
   a {
+    display: flex;
+    align-items: center;
     padding: 20px;
     transition: color 0.2s ease-in;
-    display: block;
   }
   &:hover {
     a {
@@ -39,6 +40,12 @@ const Title = styled.h1`
 const Loader = styled.span`
   tex-align: center;
   display: block;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
 `;
 
 interface CoinInterFace {
@@ -74,7 +81,12 @@ function Coins() {
         <CoinList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+              <Link to={`/${coin.id}`}>
+                <Img
+                  src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                />
+                {coin.name} &rarr;
+              </Link>
             </Coin>
           ))}
         </CoinList>
