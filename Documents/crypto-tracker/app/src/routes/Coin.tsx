8177827795db3edf920673000750bfef32dcc463
park <goslwjddl@naver.com>
2023-03-13 17:06:59 +0900

    feat :2가지 추가 컴포넌트 생성 및 coin page 데이터 시각화

--- Documents/crypto-tracker/app/src/routes/Coin.tsx
+++ Documents/crypto-tracker/app/src/routes/Coin.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useParams, Outlet} from "react-router-dom";
 import styled from "styled-components";
 const Container = styled.div`
   padding: 0px 20px;
@@ -20,6 +20,27 @@ const Title = styled.h1`
 const Loader = styled.span`
   tex-align: center;
   display: block;
+`;
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
 `;
 interface RouteParams {
   [key: string]: string | undefined; //명시적 속성의 모든 타입을 가지고 있어야 함
@@ -130,15 +151,47 @@ function Coin() {
       ).json();
       setInfo(infoData);
       setPriceInfo(priceData);
+      setLoding(false);
     })();
-  }, []);
+  }, [coinId]);
 
   return (
     <Container>
       <Header>
-        <Title>{state?.name || "Loading..."}</Title>
+        <Title>{state?.name ? state.name : loading? "Loding..." : info?.name}</Title>
+      {/* 여기info는 API로 받아오는 infoData이다 */}
       </Header>
-      {loading ? <Loader>Loading...</Loader> : null}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+        </>
+      )}
     </Container>
   );
 }
